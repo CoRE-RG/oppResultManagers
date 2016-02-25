f4a9419411bbf0cff457fb3a054e2020e1566675
Till Steinbach <till.steinbach@informatik.haw-hamburg.de>
2016-02-25 15:55:41 +0100

    documentation

---
+++ src/oppResultManagers_index.h
@@ -0,0 +1,31 @@
+// This file is used only during the doxygen documentation generation.
+
+/**
+ * @defgroup Tests Tests
+ */
+
+/**
+ * @page whatsnew What's New
+ * @verbinclude WHATSNEW
+ *
+ * @page readme Readme
+ * @verbinclude README
+ *
+ * @page install Install help
+ * @verbinclude INSTALL
+ */
+
+/**
+ * @mainpage Sourcecode Documentation - OMNeT++ Result Managers
+ *
+ * This is the sourcecode documentation for the oppResultManager source.
+ *
+ * If you are looking for more general information please refere to the <a href="../neddoc/index.html" target="_top">NED documentation</a>
+ */
+
+/**
+ * @brief Base namespace for the simulation model
+ */
+namespace CoRE4INET{
+}
+}
